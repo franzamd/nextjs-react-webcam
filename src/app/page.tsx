302d157ed2727a71d67941b63f09c3b61f22d0e0
Franz Medrano <franz.medrano@icloud.com>
2024-07-02 14:29:01 -0400

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,176 @@
-import Image from "next/image";
+'use client';
+import { useState, useRef, useCallback, useEffect } from 'react';
+import WebCam from 'react-webcam';
 
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+const HomePage = () => {
+  const [imageSrc, setImageSrc] = useState(null);
+  const webcamRef = useRef(null);
+  const [facingMode, setFacingMode] = useState<'user' | 'environment'>(
+    'environment'
   );
-}
+  const [capturing, setCapturing] = useState(false);
+  const mediaRecorRef = useRef(null);
+  const [recordedChunks, setRecordedChunks] = useState([]);
+
+  const [devices, setDevices] = useState([]);
+  const [selectedDevice, setSelectedDevice] = useState(null);
+
+  const handelDevices = useCallback((mediaDevices) => {
+    setDevices(mediaDevices.filter(({ kind }) => kind === 'videoinput'));
+  }, []);
+
+  useEffect(() => {
+    navigator.mediaDevices.enumerateDevices().then(handelDevices);
+  }, []);
+
+  function capture() {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImageSrc(imageSrc);
+  }
+
+  const handleStartCaptureClick = useCallback(() => {
+    setCapturing(true);
+    mediaRecorRef.current = new MediaRecorder(webcamRef.current.stream, {
+      mimeType: 'video/webm',
+    });
+    mediaRecorRef.current.addEventListener(
+      'dataavailable',
+      handleDataAvailable
+    );
+
+    mediaRecorRef.current.start();
+  }, [webcamRef]);
+
+  const handleDataAvailable = useCallback(
+    ({ data }) => {
+      if (data.size > 0) {
+        setRecordedChunks((prev) => prev.concat(data));
+      }
+    },
+    [setRecordedChunks]
+  );
+
+  const handleStopCaptureClick = useCallback(() => {
+    mediaRecorRef.current.stop();
+    setCapturing(false);
+  }, []);
+
+  const handleDownload = useCallback(() => {
+    if (recordedChunks.length) {
+      const blob = new Blob(recordedChunks, { type: 'video/webm' });
+      const url = URL.createObjectURL(blob);
+
+      const a = document.createElement('a');
+      document.body.appendChild(a);
+      a.href = url;
+      a.download = 'react-webcam-stream-capture.webm';
+      a.click();
+
+      window.URL.revokeObjectURL(url);
+      setRecordedChunks([]);
+    }
+  }, [recordedChunks]);
+
+  return (
+    <div className='flex h-screen flex-col md:flex-row p-10 md:p-0 gap-y-2 items-center justify-center w-full'>
+      <div>
+        <WebCam
+          audio={false}
+          height={360}
+          width={720}
+          screenshotFormat='image/png'
+          ref={webcamRef}
+          videoConstraints={{
+            height: 1080,
+            width: 1920,
+            facingMode: facingMode,
+            deviceId: selectedDevice,
+          }}
+        />
+        <button
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4'
+          onClick={capture}
+        >
+          Capture photo
+        </button>
+        <button
+          className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4'
+          onClick={() => {
+            setFacingMode(facingMode === 'user' ? 'environment' : 'user');
+          }}
+        >
+          Change Camera
+        </button>
+
+        {capturing ? (
+          <button
+            onClick={handleStopCaptureClick}
+            className='bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4'
+          >
+            Stop capturing
+          </button>
+        ) : (
+          <button
+            onClick={handleStartCaptureClick}
+            className='bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4'
+          >
+            Start capturing
+          </button>
+        )}
+
+        {devices.length > 0 && (
+          <select
+            onChange={(e) => selectedDevice(e.target.value)}
+            className='bg-white text-black font-bold py-2 px-4 rounded0full mt-4'
+          >
+            {devices.map((device, index) => (
+              <option key={index} value={device.deviceId}>
+                {device.label || `Camera ${index + 1}`}
+              </option>
+            ))}
+          </select>
+        )}
+      </div>
+      <div>
+        {imageSrc && (
+          <img
+            src={imageSrc}
+            className='w-1/2 h-1/2 object-cover rounded-lg shadow-lg'
+          />
+        )}
+        {recordedChunks.length > 0 && (
+          <video
+            controls
+            className='w-1/2 h-1/2 object-cover rounded-lg shadow-lg'
+          >
+            <source
+              src={URL.createObjectURL(
+                new Blob(recordedChunks, { type: 'video/webm' })
+              )}
+            />
+          </video>
+        )}
+
+        {recordedChunks.length > 0 && (
+          <button
+            onClick={handleDownload}
+            className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4'
+          >
+            Download
+          </button>
+        )}
+
+        {recordedChunks.length > 0 && (
+          <button
+            onClick={() => setRecordedChunks([])}
+            className='bg-red-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full mt-4'
+          >
+            Clear
+          </button>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
